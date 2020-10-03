c5e483fe71cf2a1d9d2fbc7d9f94d788c15c466e
dell <Jin-YZZ@outlook.com>
2020-10-03 21:33:04 +0800

    2020-10-13第一次提交

---
+++ rpc-01/src/main/java/com/jin/Order.java
@@ -0,0 +1,19 @@
+package com.jin;
+
+/**
+ * creat by Jin 2020/10/3 21:32
+ *
+ * @Description:
+ */
+public class Order {
+    private int age;
+    private String name;
+
+    public Order(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Order( ) {
+    }
+}
